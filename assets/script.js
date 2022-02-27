32812ae10f1b06d4911ed4a4369ca7d0ad12b4f5
Echeta Okeke <echeta@rogers.com>
2022-02-27 01:00:20 -0500

    added character randomization logic

--- assets/script.js
+++ assets/script.js
@@ -4,81 +4,50 @@
 var generatePassword = function() {
 
 
-
-
-
-
-
-
-  // Call the Password Length function
-  fnPasswordLength();
-
-    // Call the Password Lowercase function
-    fnPasswordLower();
-
-
-
-
-
-  // Allow Uppercase characters?
-//  var passwordUpper = window.prompt('Allow UPPERCASE characters? Choose 1 for YES and 2 for NO');
-
-  // Allow Numeric characters?
- // var passwordNumeric = window.prompt('Allow NUMERIC characters? Choose 1 for YES and 2 for NO');
-
-  // Allow Special Characters?
- // var passwordSpecial = window.prompt('Allow SPECIAL characters? Choose 1 for YES and 2 for NO');
-
-  var test="mama";
-  return test;
-}
-
-
-
 // Determine Password Length
-  var fnPasswordLength = function(){
-    var passwordLength = window.prompt('Choose a Password LENGTH between 8 and 128 characters.');
+var fnPasswordLength = function(){
+  var passwordLength = window.prompt('Choose a Password LENGTH between 8 and 128 characters.');
 
 
-    //Verify a number is selected
-    if (isNaN(passwordLength)) {
-      window.alert("You need to provide a valid number! Please try again.");
-      return fnPasswordLength();
-    }
-
-    // Verify that number is an integer
-    if ((passwordLength - Math.floor(passwordLength)) !== 0) {
-      window.alert("You need to provide a whole number without decimals! Please try again.");
-      return fnPasswordLength();
-    }
-    
-    // Verify number between 8 and 128 is selected
-    if (passwordLength < 8 || passwordLength > 128) {
-      window.alert("You need to provide a length between 8 and 128 characters! Please try again.");
-      return fnPasswordLength();
-    } 
-
-    return passwordLength;
+  //Verify a number is selected
+  if (isNaN(passwordLength)) {
+    window.alert("You need to provide a valid number! Please try again.");
+    return fnPasswordLength();
   }
+
+  // Verify that number is an integer
+  if ((passwordLength - Math.floor(passwordLength)) !== 0) {
+    window.alert("You need to provide a whole number without decimals! Please try again.");
+    return fnPasswordLength();
+  }
+  
+  // Verify number between 8 and 128 is selected
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("You need to provide a length between 8 and 128 characters! Please try again.");
+    return fnPasswordLength();
+  } 
+
+  return passwordLength;
+}
 
 
 // Allow Lowercase characters?
 var fnPasswordLower = function() {
 
-  var passwordLower = window.prompt('Allow LOWERCASE characters? Choose 1 for YES and 2 for NO');
-  
-  // Convert to integer
-  passwordLower = parseInt(passwordLower);
-  switch (passwordLower) {
-    case 1:
-      return 1;
+var passwordLower = window.prompt('Allow LOWERCASE characters? Choose 1 for YES and 2 for NO');
 
-    case 2:
-      return 0;
+// Convert to integer
+passwordLower = parseInt(passwordLower);
+switch (passwordLower) {
+  case 1:
+    return 1;
 
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      fnPasswordLower();
+  case 2:
+    return 0;
+
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+    fnPasswordLower();
   }
 }
 
@@ -86,20 +55,20 @@ var fnPasswordLower = function() {
 // Allow Uppercase characters?
 var fnPasswordUpper = function() {
 
-  var passwordUpper = window.prompt('Allow UPPERCASE characters? Choose 1 for YES and 2 for NO');
-  
-  // Convert to integer
-  passwordUpper = parseInt(passwordUpper);
-  switch (passwordUpper) {
-    case 1:
-      return 1;
+var passwordUpper = window.prompt('Allow UPPERCASE characters? Choose 1 for YES and 2 for NO');
 
-    case 2:
-      return 0;
+// Convert to integer
+passwordUpper = parseInt(passwordUpper);
+switch (passwordUpper) {
+  case 1:
+    return 1;
 
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      fnPasswordUpper();
+  case 2:
+    return 0;
+
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+    fnPasswordUpper();
   }
 }
 
@@ -107,20 +76,20 @@ var fnPasswordUpper = function() {
 // Allow Numeric characters?
 var fnPasswordNumeric = function() {
 
-  var passwordNumeric = window.prompt('Allow NUMERIC characters? Choose 1 for YES and 2 for NO');
-  
-  // Convert to integer
-  passwordNumeric = parseInt(passwordNumeric);
-  switch (passwordNumeric) {
-    case 1:
-      return 1;
+var passwordNumeric = window.prompt('Allow NUMERIC characters? Choose 1 for YES and 2 for NO');
 
-    case 2:
-      return 0;
+// Convert to integer
+passwordNumeric = parseInt(passwordNumeric);
+switch (passwordNumeric) {
+  case 1:
+    return 1;
 
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      fnPasswordNumeric();
+  case 2:
+    return 0;
+
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+    fnPasswordNumeric();
   }
 }
 
@@ -128,36 +97,82 @@ var fnPasswordNumeric = function() {
 // Allow Special characters?
 var fnPasswordSpecial = function() {
 
-  var passwordSpecial = window.prompt('Allow SPECIAL characters? Choose 1 for YES and 2 for NO');
-  
-  // Convert to integer
-  passwordSpecial = parseInt(passwordSpecial);
-  switch (passwordSpecial) {
-    case 1:
-      return 1;
+var passwordSpecial = window.prompt('Allow SPECIAL characters? Choose 1 for YES and 2 for NO');
 
-    case 2:
-      return 0;
+// Convert to integer
+passwordSpecial = parseInt(passwordSpecial);
+switch (passwordSpecial) {
+  case 1:
+    return 1;
 
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-      fnPasswordSpecial();
+  case 2:
+    return 0;
+
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+    fnPasswordSpecial();
   }
 }
 
 
-
 // Password Preference Parameters (1-True, 0-False)
 var passwordType = {
-  length: fnPasswordLength(),
-  lowercase: fnPasswordLower(),
-  uppercase: fnPasswordUpper(),
-  numeric: fnPasswordNumeric(),
-  special: fnPasswordSpecial(),
+length: fnPasswordLength(),
+lowercase: fnPasswordLower(),
+uppercase: fnPasswordUpper(),
+numeric: fnPasswordNumeric(),
+special: fnPasswordSpecial(),
 };
 
+//validation to ensure at least one charatcer type is selected
+if ((passwordType.lowercase + passwordType.uppercase + passwordType.numeric + passwordType.special) < 1) {
+  window.alert("You must select at least one character type for your password! Try again.")
+  return generatePassword();
+} 
+console.log(passwordType);
 
-//console.log(passwordType);
+
+// Declaring all characters
+const lowerChar = 'abcdefghijklmnopqrstuvwxyz';
+const upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numChar = '0123456789';
+const specialChar =  '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~ ';
+
+var genPassword = '';
+var possibleChar = '';
+
+
+if (passwordType.lowercase === 1) {
+  possibleChar += lowerChar;
+}
+
+
+if (passwordType.uppercase === 1) {
+  possibleChar += upperChar;
+}
+
+
+if (passwordType.numeric === 1) {
+  possibleChar += numChar;
+}
+
+if (passwordType.special === 1) {
+  possibleChar += specialChar;
+}
+
+// Get the total number of possible characters
+var charLength = possibleChar.length;  
+
+// Loop through and add a random character at a time
+for (var i = 0; i < passwordType.length; i++) {
+  genPassword += possibleChar.charAt(Math.floor(Math.random() * charLength));
+}
+
+return genPassword;
+
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
