95b08aea1a9f0500ab918752aab6f19af3f03bfd
Echeta Okeke <echeta@rogers.com>
2022-02-26 22:38:02 -0500

    added logic for selecting character length

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# password-generator
+# Password Generator
+
+## Purpose
+This is a Password Generator that generates a random password based on the user inputs
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Wesbite
+https://shookspear.github.io/password-generaotor/
+
+## Contribution
+Made with ❤️ by Echeta
+
+### ©️2022 Echeta Okeke
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,53 @@
-// Assignment code here
 
+
+// Generate the Password
+var generatePassword = function() {
+
+  // Determine Password Length
+    var fnPasswordLength = function(){
+      var passwordLength = window.prompt('Choose a Password LENGTH between 8 and 128 characters.');
+
+
+      //Verify a number is selected
+      if (isNaN(passwordLength)) {
+        window.alert("You need to provide a valid number! Please try again.");
+        return fnPasswordLength();
+      }
+
+      // Verify that number is an integer
+      if ((passwordLength - Math.floor(passwordLength)) !== 0) {
+        window.alert("You need to provide a whole number without decimals! Please try again.");
+        return fnPasswordLength();
+      }
+      
+      // Verify number between 8 and 128 is selected
+      if (passwordLength < 8 || passwordLength > 128) {
+        window.alert("You need to provide a length between 8 and 128 characters! Please try again.");
+        return fnPasswordLength();
+      } 
+    }
+
+  // Call the Password Length function
+  fnPasswordLength();
+
+
+
+  
+  // Allow Lowercase characters?
+ // var passwordLower = window.prompt('Allow LOWERCASE characters? Choose 1 for YES and 2 for NO');
+
+  // Allow Uppercase characters?
+//  var passwordUpper = window.prompt('Allow UPPERCASE characters? Choose 1 for YES and 2 for NO');
+
+  // Allow Numeric characters?
+ // var passwordNumeric = window.prompt('Allow NUMERIC characters? Choose 1 for YES and 2 for NO');
+
+  // Allow Special Characters?
+ // var passwordSpecial = window.prompt('Allow SPECIAL characters? Choose 1 for YES and 2 for NO');
+
+  var test="mama";
+  return test;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
